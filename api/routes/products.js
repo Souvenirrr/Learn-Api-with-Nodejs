2c87892968654f437b7d30faafe47eb953967034
Souvenirrr <tungtran130@gmail.com>
2020-09-22 14:09:55 +0700

    Learn make RESTful Api

---
+++ api/routes/products.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+// get products
+router.get('/', (req, res, next) => {
+	res.status(200).json({
+		message: 'Success gets'
+	});
+});
+
+router.post('/', (req, res, next) => {
+	res.status(200).json({
+		message: 'Success post'
+	});
+});
+
+router.get('/:productId', (req, res, next) => {
+	const id = req.params.productId;
+	if (id == '12') {
+		res.status(200).json({
+			message: 'You discovered the special ID',
+			id: id
+		});
+	} else {
+		res.status(200).json({
+			message: 'You can pass'
+		})
+	}
+});
+
+router.patch('/:productId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Updated product!'
+	});
+});
+
+router.delete('/:productId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Deleted product!'
+	});
+});
+
+module.exports = router
